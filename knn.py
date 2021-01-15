626b71d3ac51a7b6ac985ce9bcb8ea6d37e428c7
sonyeonggwang <syoungk20@naver.com>
2021-01-15 09:12:27 +0900

    first commit

---
+++ knn.py
@@ -0,0 +1,18 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import simpledatasetloader
+import simplepreprocessor
+from imutils import paths
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, 
+    help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1, 
+    help="# of nearest neighbors for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1, 
+    help="# of jobs for k-NN distance (-1 uses all available cores)")
+args = vars(ap.parse_args())
+
---
+++ simpledatasetloader.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+import os
+
+''' split exmple
+    >>> a = "Life is too short, You need Python"
+    >>> a[0]
+    'L'
+    >>> a[12]
+    's'
+    >>> a[-1]
+    'n'
+'''
+
+class SimpleDatasetLoader:
+    def __init__(self, preprocessors=None):
+        self.preprocessors = preprocessors
+
+        if self.preprocessors is None:
+            self.preprocessors = []
+
+    def load(self, imagePaths, Verbose=-1):
+        data = []
+        labels = []
+
+        for (i, imagePath) in enumerate(imagePaths):
+            image = cv2.imread(imagePath)
+            label = imagePath.split(os.path.sep)[-2]
+
+            if self.preprocessors is not None:
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
+            
+            data.append(image)
+            labels.append(label)
+
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(imagepaths)))
+
+        return (np.array(data), np.array(labels))
